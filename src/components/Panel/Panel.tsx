9c2bddbf5a1d432574e641fbf52152ad8e59bb3d
Alexander <alexandr.rostow@gmail.com>
2022-07-14 21:44:38 +0700

    first commit

--- src/components/Panel/Panel.tsx
+++ src/components/Panel/Panel.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
-import {Box, TextField} from '@mui/material';
+import {Add as AddIcon } from '@mui/icons-material';
+import {Button, Paper, TextField} from '@mui/material';
 
 export const Panel = () => {
+  const [inputValue, setInputValue] = React.useState('');
+
+  const onAddClick = () => {
+    console.log('@', inputValue)
+    setInputValue('');
+  }
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {value} = event.target;
+    setInputValue(value);
+  }
+
+
   return (
-    <Box>
-      <TextField />
-    </Box>
+    <Paper elevation={2}
+           sx={{padding: '15px 30px',
+                borderRadius: 2,
+                display: 'flex',
+
+                alignContent: 'center',
+                gap: 2 }}>
+      <TextField value={inputValue}
+                 onChange={onChange}
+                 label='Todo name'/>
+      <Button startIcon={<AddIcon />}
+              onClick={onAddClick}
+              variant="outlined">
+        Add Todo
+      </Button>
+    </Paper>
   )
 };
